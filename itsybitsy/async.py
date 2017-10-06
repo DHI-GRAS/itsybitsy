cba086ebb876f87995a4d3eccc97686adf603288
Dion Häfner <mail@dionhaefner.de>
2017-10-06 09:54:21 +0200

    add asynchronous version of crawler

--- itsybitsy/async.py
+++ itsybitsy/async.py
@@ -1,2 +1,36 @@
-async def:
-    pass
+def crawl_website(base_url, only_go_deeper=True, max_depth=5):
+    visited = set()
+
+    real_base_url = get_real_url(base_url)
+
+    async def visit_link(page_url, page_depth):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(page_url) as response:
+                if 'text/html' not in response.headers['content-type']:
+                    return None
+                real_page_url = str(response.url)
+                parser = LinkFinder(real_page_url)
+                parser.feed(await response.text())
+                new_links = []
+                for link in parser.links:
+                    if only_go_deeper and not url_is_deeper(link, real_base_url):
+                        continue
+                    if max_depth and page_depth >= max_depth:
+                        continue
+                    if link in visited:
+                        continue
+                    visited.add(link)
+                    new_links.append((link, page_depth+1))
+                return new_links
+
+    loop = asyncio.get_event_loop()
+    links_to_process = [(real_base_url, 0)]
+    while links_to_process:
+        for link, depth in links_to_process:
+            yield link
+        links_to_process = loop.run_until_complete(
+                                asyncio.gather(
+                                    *[visit_link(link, depth) for link, depth in links_to_process]
+                                )
+        )
+        links_to_process = [item for sublist in links_to_process for item in sublist if item]
--- itsybitsy/util.py
+++ itsybitsy/util.py
@@ -1,8 +1,19 @@
 import urlparse
+import requests
+
+
+def get_real_url(url):
+    with requests.Session() as session:
+        with session.get(url) as response:
+            return response.url
 
 
 def is_relative_link(url):
     return not urlparse.urlparse(url).netloc
+
+
+def link_is_http(url):
+    return urlparse.urlparse(url).scheme.startswith("http")
 
 
 def normalize_url(url):
@@ -22,14 +33,13 @@ def url_is_deeper(child, parent, normalizer=url_normalize.url_normalize):
 
 class LinkFinder(HTMLParser):
     """Parses an HTML file and build a list of links.
-
     Links are stored into the 'links' list. If `base_url` is given, relative
     links are resolved into absolute paths.
     """
     def __init__(self, base_url=None, link_tag='a', link_attr='href'):
         HTMLParser.__init__(self)
 
-        self._base_url = base_url
+        self._base_url = normalize_url(base_url)
         self._link_tag = link_tag
         self._link_attr = link_attr
         self.links = []
@@ -45,8 +55,15 @@ class LinkFinder(HTMLParser):
 
                 if self._base_url and is_relative_link(value):
                     value = urlparse.urljoin(self._base_url, value)
-                value = normalize_url(value)
+
+                if not link_is_http(value):
+                    continue
+
+                try:
+                    value = normalize_url(value)
+                except ValueError:
+                    continue
 
                 if value not in self._seen_links:
-                    self.links.add(value)
+                    self.links.append(value)
                     self._seen_links.add(value)
